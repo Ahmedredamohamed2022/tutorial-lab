06ea318833576b6e26464bca51b51f4b32b79f3b
Ahmedredamohamed2022 <105387946+Ahmedredamohamed2022@users.noreply.github.com>
2022-10-22 19:53:50 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Manual Lab'
+copyright = '2022, Efabless'
+author = 'Ahmed Reda'
 
 release = '0.1'
 version = '0.1.0'
